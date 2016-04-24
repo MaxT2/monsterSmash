cb35bc0c7caea5d6d0fdcbbef43ce9d96fd0a7c2
MaxT2 <tristen@payne.org>
2016-04-24 02:25:41 -0400

    created getters and setters

--- monsterSmash/src/monsterSmash/Creature.java
+++ monsterSmash/src/monsterSmash/Creature.java
@@ -4,11 +4,49 @@ package monsterSmash;
 
 public class Creature {
 	
-private short healthStat;
-private short physicalAttackStat;
-private short energyAttackStat;
-private short physicalDefenseStat; 
-private short energyDefenseStat;
-private short speedStat; 
+	private short physicalAttackStat;
+	private short energyAttackStat;
+	private short physicalDefenseStat; 
+	private short energyDefenseStat;
+	private short speedStat; 
+	private short healthStat;
+
+public short getHealthStat() {
+	return healthStat;
+}
+public void setHealthStat(short healthStat) {
+	this.healthStat = healthStat;
+}
+public short getPhysicalAttackStat() {
+	return physicalAttackStat;
+}
+public void setPhysicalAttackStat(short physicalAttackStat) {
+	this.physicalAttackStat = physicalAttackStat;
+}
+public short getEnergyAttackStat() {
+	return energyAttackStat;
+}
+public void setEnergyAttackStat(short energyAttackStat) {
+	this.energyAttackStat = energyAttackStat;
+}
+public short getPhysicalDefenseStat() {
+	return physicalDefenseStat;
+}
+public void setPhysicalDefenseStat(short physicalDefenseStat) {
+	this.physicalDefenseStat = physicalDefenseStat;
+}
+public short getEnergyDefenseStat() {
+	return energyDefenseStat;
+}
+public void setEnergyDefenseStat(short energyDefenseStat) {
+	this.energyDefenseStat = energyDefenseStat;
+}
+public short getSpeedStat() {
+	return speedStat;
+}
+public void setSpeedStat(short speedStat) {
+	this.speedStat = speedStat;
+}
+
 	
 }
